5db57ad7348eb2fd1cbe1faec7309b205468cab0
AndaZidele <87810405+AndaZidele@users.noreply.github.com>
2021-11-23 14:57:16 +0200

    Initial Commit

---
+++ FitnessApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FitnessApplication
+//
+//  Created by anda.zidele on 23/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
